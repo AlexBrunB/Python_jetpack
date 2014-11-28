6b0f695dc79396d34b2bef8443966360ecf073d0
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-11-28 14:26:14 +0100

    Code Formating and TODO.

--- exemples/jetpack.py
+++ exemples/jetpack.py
@@ -13,28 +13,29 @@ from cocos.actions import *
 import cocos
 from cocos.actions import *
 
+
 class MainMenu(Menu):
     def __init__(self):
-        super( MainMenu, self).__init__("JetPack Python")
+        super(MainMenu, self).__init__("JetPack Python")
 
         self.menu_valign = CENTER
         self.menu_halign = CENTER
 
         items = [
-            (MenuItem('Jouer', self.on_play ) ),
-            (MenuItem('Options', self.on_options ) ),
-            (MenuItem('Quitter', self.on_quit )),
+            (MenuItem('Jouer', self.on_play)),
+            (MenuItem('Options', self.on_options)),
+            (MenuItem('Quitter', self.on_quit)),
         ]
 
-        self.create_menu( items, shake(), shake_back() )
+        self.create_menu(items, shake(), shake_back())
 
-    def on_play(self ):
+    def on_play(self):
         pyglet.app.run()
 
     def on_options(self):
         self.parent.switch_to(1)
 
-    def on_quit(self ) :
+    def on_quit(self):
         pyglet.app.exit()
 
 
@@ -42,27 +43,35 @@ class OptionsMenu(cocos.layer.Layer):
     def __init__(self):
         super(OptionsMenu, self).__init__()
 
-        label = cocos.text.Label('Options',
+        label = cocos.text.Label(
+            'Options',
             font_name='Ubuntu Condensed',
             font_size=64,
-            anchor_x='center', anchor_y='center')
+            anchor_x='center', anchor_y='center'
+        )
 
-        label.position = 300,400
-        self.add( label )
+        label.position = 300, 400
+        self.add(label)
+
 
 def main():
     pyglet.font.add_directory('.')
 
-    director.init( resizable=True)
+    director.init(resizable=True)
 
-    layer = cocos.layer.ColorLayer(192,192,192,80)
+    layer = cocos.layer.ColorLayer(192, 192, 192, 80)
 
-    main_scene = cocos.scene.Scene (layer)
+    main_scene = cocos.scene.Scene(layer)
 
-
-    director.run ( Scene(MultiplexLayer( MainMenu(),OptionsMenu()) ))
+    director.run(
+        Scene(
+            MultiplexLayer(
+                MainMenu(),
+                OptionsMenu()
+            )
+        )
+    )
 
 
 if __name__ == "__main__":
     main()
-
