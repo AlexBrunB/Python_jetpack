8e43b33e6c039f360bcad5b17c2a2892ae93e228
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-12-05 23:10:05 +0100

    Add action FadeIn on first level introduction.

--- pyjetpack/level1.py
+++ pyjetpack/level1.py
@@ -1,28 +1,17 @@
 
 import cocos
-from cocos.director import director
 from cocos.sprite import Sprite
+from cocos.actions import FadeIn
+
 
 
 class BackgroundLayer(cocos.layer.ColorLayer):
     def __init__(self):
-        super(BackgroundLayer, self).__init__( 192,192,192,80)
-
-        x,y = director.get_window_size()
-
-        label = cocos.text.Label('Python JetPack',
-            font_name='Ubuntu Condensed',
-            font_size=32,
-            anchor_x='center', anchor_y='center')
-
-        label.position = 120, 320
-        self.add(label)
+        super(BackgroundLayer, self).__init__( 192,192,192,192)
 
         sprite = cocos.sprite.Sprite('trees.png')
-        sprite.position = 220,220
-        sprite.opacity = 150
-        sprite.scale = 1
-
+        sprite.position = 220, 220
+        sprite.do(FadeIn(3))
 
 
         self.add(sprite, z=0)
