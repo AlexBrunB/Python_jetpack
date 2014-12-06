f131daed7a5bf0f3cec304f92af385d121efbe6a
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-12-06 16:47:32 +0100

    Finish level1 Introduction

--- pyjetpack/level1.py
+++ pyjetpack/level1.py
@@ -1,31 +1,39 @@
 
 import cocos
+from cocos.director import director
 from cocos.sprite import Sprite
-from cocos.actions import FadeIn
-from cocos import tiles, actions, layer
-import pyglet
-from pyglet.window import key
+from cocos.actions import FadeIn, MoveBy
+
 
 
 class BackgroundLayer(cocos.layer.ColorLayer):
     def __init__(self):
         super(BackgroundLayer, self).__init__( 192,192,192,192)
 
+        x,y = director.get_window_size()
+
         label = cocos.text.Label('Level 1',
-        font_name='Ubuntu Condensed',
-        font_size=64,
-        anchor_x='center', anchor_y='center')
+            font_name='Ubuntu Condensed',
+            font_size=70,
+            anchor_x='center', anchor_y='center')
 
         label.position = 120, 320
+        label.do(MoveBy( (600,0), 2 ))
         self.add(label)
 
-        sprite = cocos.sprite.Sprite('trees.png')
-        sprite.position = 220, 220
-        sprite.do(FadeIn(3))
+        sprite1 = Sprite('trees.png')
+        self.add( sprite1 )
+        sprite1.position = 220, 200
+        sprite1.do(FadeIn(3))
+
+        #sprite2 = Sprite('car.gif')
+        #sprite1.add( sprite2 )
+        #sprite2.position = -50, -50
+        #sprite2.scale = 3
 
 
-        self.add(sprite, z=0)
-        
+        self.add(sprite1)
+
 
 def get_newgame():
 
