3de912fb455845e75002736766923f9ed512df7f
Florent Aide <florent.aide@gmail.com>
2014-12-08 19:58:40 +0100

    cleanup & sound support

--- pyjetpack/cli.py
+++ pyjetpack/cli.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 
 
@@ -8,24 +9,21 @@ from cocos.menu import MenuItem, MultiplexLayer
 from cocos.menu import shake, shake_back
 from level1 import get_newgame
 from cocos.director import director
+from pyjetpack import soundex
 
 
 class MainMenu(Menu):
     def __init__(self):
         super(MainMenu, self).__init__("JetPack Python")
 
-        #select_sound = ('space_oddity.mp3')
+        soundex.set_music('space_oddity.mp3')
 
         items = [
             (MenuItem('Jouer', self.on_new_game)),
             (MenuItem('Options', self.on_options)),
             (MenuItem('Quitter', self.on_quit)),
         ]
-        #select_sound('space_oddity.mp3')
         self.create_menu(items, shake(), shake_back())
-
-        #if select_sound:
-        #    select_sound.play('space_oddity.mp3')
 
     def on_new_game(self):
         director.push(get_newgame())
@@ -41,12 +39,12 @@ class OptionMenu(Menu):
     def __init__(self):
         super(OptionMenu, self).__init__("JetPack Python")
 
-        l = []
-        l.append( MenuItem('Quit', self.on_quit))
-        l.append( MenuItem('Volumes', self.on_quit) )
+        l = [
+            MenuItem('Quit', self.on_quit),
+            MenuItem('Volumes', self.on_quit),
+        ]
 
         self.create_menu(l)
-
 
     def on_quit(self):
         self.parent.switch_to(0)
@@ -56,9 +54,7 @@ def main():
 
     window = director.init(resizable=True)
     director.window = window
-    scene = Scene(
-        MultiplexLayer(MainMenu(), OptionMenu())
-        )
+    scene = Scene(MultiplexLayer(MainMenu(), OptionMenu()))
     director.run(scene)
 
 if __name__ == "__main__":
--- pyjetpack/level1.py
+++ pyjetpack/level1.py
@@ -1,46 +1,44 @@
-
-import cocos
-from cocos.director import director
+# -*- encoding: utf-8 -*-
+from cocos.scene import Scene
+from cocos.text import Label
+from cocos.layer import ColorLayer
 from cocos.sprite import Sprite
 from cocos.actions import FadeIn, MoveBy
 
 
-
-class BackgroundLayer(cocos.layer.ColorLayer):
+class BackgroundLayer(ColorLayer):
     def __init__(self):
-        super(BackgroundLayer, self).__init__( 192,192,192,192)
+        super(BackgroundLayer, self).__init__(192, 192, 192, 192)
 
-        x,y = director.get_window_size()
-
-        label = cocos.text.Label('Level 1',
+        label = Label(
+            'Level 1',
             font_name='Ubuntu Condensed',
             font_size=70,
-            anchor_x='center', anchor_y='center')
+            anchor_x='center',
+            anchor_y='center'
+        )
 
         label.position = 120, 320
-        label.do(MoveBy( (600,0), 2 ))
+        label.do(MoveBy((600, 0), 2))
         self.add(label)
 
         sprite1 = Sprite('trees.png')
-        self.add( sprite1 )
+        self.add(sprite1)
         sprite1.position = 220, 220
         sprite1.do(FadeIn(3))
 
         sprite2 = Sprite('car.gif')
-        sprite1.add( sprite2 )
+        sprite1.add(sprite2)
         sprite2.position = -100, -50
-        sprite2.do(MoveBy((300,0), 1)),
+        sprite2.do(MoveBy((300, 0), 1)),
         sprite2.scale = 3
 
-
-        self.add(sprite1,sprite2)
+        self.add(sprite1, sprite2)
 
 
 def get_newgame():
-
     layer = BackgroundLayer()
-
-    return cocos.scene.Scene (layer)
+    return Scene(layer)
 
 
 
