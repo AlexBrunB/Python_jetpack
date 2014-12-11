1979e32e4ee9b0072d89f58b88861dc047910675
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-12-11 17:49:11 +0100

    Introduce EventDispatcher and Listener

--- pyjetpack/level1.py
+++ pyjetpack/level1.py
@@ -5,6 +5,8 @@ from cocos.text import Label
 from cocos.layer import ColorLayer
 from cocos.sprite import Sprite
 from cocos.actions import FadeIn, MoveBy
+from cocos.director import director
+import pyglet
 
 
 class BackgroundLayer(ColorLayer):
@@ -32,13 +34,11 @@ class BackgroundLayer(ColorLayer):
         sprite1.position = 220, 220
         sprite1.do(FadeIn(3))
 
-
         self.add(sprite1, z=0)
-
 
         self.posx = 100
         self.posy = 240
-        self.text = Label('No mouse events yet', font_size=18, x=self.posx, y=self.posy )
+        self.text = Label('Mouse Event', font_size=18, x=self.posx, y=self.posy )
         self.add( self.text )
 
 
@@ -52,13 +52,39 @@ class BackgroundLayer(ColorLayer):
         self.update_text (x, y)
 
     def on_mouse_drag (self, x, y, dx, dy, buttons, modifiers):
-
         self.update_text (x, y)
 
     def on_mouse_press (self, x, y, buttons, modifiers):
         self.posx, self.posy = director.get_virtual_coordinates (x, y)
         self.update_text (x,y)
 
+#This is the emitter
+"""
+class Playground(pyglet.event.EventDispatcher):
+    is_event_handler = True
+
+
+    def __init__(self):
+        super(Playground, self).__init__()
+
+    def __register_events(self):
+        self.register_event_type('on_jump')
+
+"""
+#
+#  This is the listener
+#  Listener provide actions
+#  But i'm not able allready to add function move for the Character on the Class BackgroundLayer
+#
+"""
+class Character(object):
+    def __init__(self):
+        self.sprite = []
+
+        self.posx = 100
+        self.posy = 240
+        self.add( self.Sprite('car.gif', x=self.posx, y=self.posy ) )
+"""
 
 
 
