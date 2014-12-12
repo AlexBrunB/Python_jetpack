a1f19ed25ed6f27f056014609f2778bd1f0e985e
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-12-12 13:59:17 +0100

    Improve Listener and EventDispatcher

--- pyjetpack/level1.py
+++ pyjetpack/level1.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
-
+import cocos
 from cocos.scene import Scene
 from cocos.text import Label
-from cocos.layer import ColorLayer
+from cocos.layer import ColorLayer, MultiplexLayer
 from cocos.sprite import Sprite
 from cocos.actions import FadeIn, MoveBy
 from cocos.director import director
@@ -53,39 +53,49 @@ class BackgroundLayer(ColorLayer):
         self.update_text(x, y)
 
     def on_mouse_press(self, x, y, buttons, modifiers):
-        self.posx, self.posy = director.get_virtual_coordinates (x, y)
+        self.posx, self.posy = director.get_virtual_coordinates(x, y)
         self.update_text(x, y)
 
-#This is the emitter
-"""
-class Playground(pyglet.event.EventDispatcher):
-    is_event_handler = True
 
+#This is the emitter
+class Playground(cocos.layer.ScrollableLayer, pyglet.event.EventDispatcher):
+    is_event_handler = True
 
     def __init__(self):
         super(Playground, self).__init__()
 
-    def __register_events(self):
-        self.register_event_type('on_jump')
+        x,y = director.get_window_size()
+        self.transform_anchor_x = x // 2
+        self.transform_anchor_y = y // 2
 
-"""
-#
-#  This is the listener
-#  Listener provide actions
-#  But i'm not able allready to add function move for the Character on the Class BackgroundLayer
-#
-"""
+    def __register_event_type(self):
+        self.register_event_type('on_enter')
+        self.register_event_type('on_activate')
+
+    def on_enter(self):
+        director.push_handlers(self.on_cocos_resize)
+        super(Character, self).on_enter()
+        self.parent.switch_to(1)
+
+
+#And this is the listener
+# First at all, i can't find the action to add sprite as an object or to create an object
+## And I don't figure why this class is not in BackgroundLayer
 class Character(object):
     def __init__(self):
-        self.sprite = []
 
-        self.posx = 100
-        self.posy = 240
-        self.add( self.Sprite('car.gif', x=self.posx, y=self.posy ) )
-"""
+        sprite = Sprite('car.gif')
+        sprite.position = 220, 220
+        sprite.do(FadeIn(3))
+        #self.add(sprite, z=0)
+
+    def on_activate(self):
+        playground = Playground()
+        playground.switch_to(1)
 
 
 def get_newgame():
+    scroller = cocos.layer.ScrollingManager(viewport=director.window)
+    scroller.add(Playground(), Character())
     layer = BackgroundLayer()
     return Scene(layer)
-
