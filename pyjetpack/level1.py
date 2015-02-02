e4a1005b390f84477774e3752e5dfbb4b9fd9394
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2015-02-02 18:02:01 +0100

    Fix import problem & Improve character action.

--- pyjetpack/level1.py
+++ pyjetpack/level1.py
@@ -2,22 +2,23 @@
 import cocos
 from cocos.scene import Scene
 from cocos.actions import MoveBy, FadeIn
-from cocos.sprite import Sprite
 import pyglet
 from pyglet.window import key
+
 
 keyboard = key.KeyStateHandler()
 
 
-class Character(cocos.layer.Layer):
+class Character(cocos.layer.ColorLayer):
     is_event_handler = True
 
     def __init__(self):
-        super(Character, self).__init__()
-        sprite = Sprite('data/trees.png')
-        sprite.position = 220, 220
-        sprite.opacity = 60
-        self.add(sprite, z=0)
+        super(Character, self).__init__(192, 192, 192, 80)
+        img = pyglet.image.load('data/trees.png')
+        self.sprite = cocos.sprite.Sprite(img)
+        self.sprite.position = 220, 220
+        self.sprite.opacity = 60
+        self.add(self.sprite, z=0)
 
         img = pyglet.image.load('data/kid.png')
         self.sprite = cocos.sprite.Sprite(img)
@@ -35,17 +36,21 @@ class Character(cocos.layer.Layer):
         self.label.do(MoveBy((0, 120), 4))
         self.add(self.label)
 
-
-
-    def on_key_press(self, symbol):
-        if symbol == key.UP:
-            self.sprite.y += 60
+    def on_key_press(self, symbol, modifiers):
+        print 'pressing'
+        if symbol == key.RIGHT:
+            self.sprite.x += 20
+        elif symbol == key.LEFT:
+            self.sprite.x -= 20
+        elif symbol == key.UP:
+            self.sprite.y += 20
         elif symbol == key.DOWN:
-            self.sprite.y -= 60
+            self.sprite.y -= 20
+
 
 
 def get_newgame():
 
     player = Character()
     main_scene = cocos.scene.Scene(player)
-    return Scene(main_scene)
+    return Scene(player)
