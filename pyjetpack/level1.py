ace3032e399d9e91d3e936d28085e327f75d7886
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2015-02-04 15:22:25 +0100

    Introduce Pause & ScrollableLayer

--- pyjetpack/level1.py
+++ pyjetpack/level1.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
 import cocos
 from cocos.scene import Scene
-from cocos.actions import MoveBy, FadeIn
+from cocos.sprite import Sprite
+from cocos.actions import MoveTo, MoveBy, FadeIn
 import pyglet
 from pyglet.window import key
 
 
 keyboard = key.KeyStateHandler()
-
 
 class Character(cocos.layer.ColorLayer):
     is_event_handler = True
@@ -23,8 +23,8 @@ class Character(cocos.layer.ColorLayer):
         img = pyglet.image.load('data/kid.png')
         self.sprite = cocos.sprite.Sprite(img)
         self.sprite.scale = 6
+        self.sprite.velocity = (120, 120)
         self.sprite.position = 120, 60
-        self.sprite.do(FadeIn(8))
         self.add(self.sprite)
 
         self.label = cocos.text.Label('Level 1',
@@ -37,7 +37,6 @@ class Character(cocos.layer.ColorLayer):
         self.add(self.label)
 
     def on_key_press(self, symbol, modifiers):
-        print 'pressing'
         if symbol == key.RIGHT:
             self.sprite.x += 20
         elif symbol == key.LEFT:
@@ -48,6 +47,39 @@ class Character(cocos.layer.ColorLayer):
             self.sprite.y -= 20
 
 
+class PauseScene(Scene):
+    '''Pause Scene'''
+    def __init__(self, background, *layers):
+        super(PauseScene, self).__init__(*layers)
+        self.bg = background
+        self.width, self.height = self.director.get_window_size()
+        self.director.pause();
+
+
+class ScrollableLayer(cocos.layer.Layer):
+    ''' Introduce Scrollable Layer
+        Also need the ScrollingManager'''
+    view_x, view_y = 100, 100
+    view_w, view_h = 100, 100
+    origin_x = origin_y = origin_z = 0
+    img = pyglet.image.load('data/kid.png')
+
+    def __init__(self, parallax=1):
+        super(ScrollableLayer, self).__init__()
+        self.parallax = parallax
+
+        self.transform_anchor_x = 0
+        self.transform_anchor_y = 0
+
+        self.batch = pyglet.graphics.Batch()
+
+    def on_enter(self):
+        self.director.push_handlers(self.on_cocos_resize)
+        super(ScrollableLayer, self).on_enter()
+
+    def on_exit(self):
+        super(ScrollableLayer, self).on_exit()
+        self.director.pop_handlers()
 
 def get_newgame():
 
